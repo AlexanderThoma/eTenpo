a8029914e22dd5215321b508fe654ed393a6c0ac
Alexander Thoma <thomalexander92@outlook.com>
2023-08-08 16:06:16 +0200

    feat(category): add basic methods in domain class

--- src/Services/Product/eTenpo.Product.Domain/AggregateRoots/CategoryAggregate/Category.cs
+++ src/Services/Product/eTenpo.Product.Domain/AggregateRoots/CategoryAggregate/Category.cs
@@ -1,19 +1,54 @@
 using eTenpo.Product.Domain.Common;
+using eTenpo.Product.Domain.Exceptions;
 
 namespace eTenpo.Product.Domain.AggregateRoots.CategoryAggregate;
 
 public class Category : AggregateRoot, IAuditable
 {
+    private List<ProductAggregate.Product> products = new ();
+
     public Category(CategoryName name, CategoryDescription description)
     {
         this.Name = name;
         this.Description = description;
-        this.Products = new List<Guid>();
     }
     
-    public CategoryName Name { get; init; }
-    public CategoryDescription Description { get; init; }
-    public List<Guid>? Products { get; private set; }
+    public CategoryName Name { get; private set; }
+    
+    public CategoryDescription Description { get; private set; }
+
+    public IReadOnlyCollection<ProductAggregate.Product> Products => products.AsReadOnly();
+
     public DateTime CreatedOnUtc { get; }
+    
     public DateTime? ModifiedOnUtc { get; }
+    
+    public void Delete()
+    {
+        if (this.products.Any())
+        {
+            throw new CategoryValidationException(
+                "There are still products using this category. Delete them or assign a different category");
+        }
+    }
+
+    public void UpdateName(CategoryName name)
+    {
+        if (this.Name == name)
+        {
+            return; 
+        }
+
+        this.Name = name;
+    }
+
+    public void UpdateDescription(CategoryDescription description)
+    {
+        if (this.Description == description)
+        {
+            return; 
+        }
+
+        this.Description = description;
+    }
 }
