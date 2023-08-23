3ada53147090fc614c9e846505ba82f1e4808eee
Alexander Thoma <thomalexander92@outlook.com>
2023-08-23 15:37:16 +0200

    chore(merge): fix build errors due to merge conflicts

--- src/Tests/Test.Shared/IntegrationTestWebApplicationFactory.cs
+++ src/Tests/Test.Shared/IntegrationTestWebApplicationFactory.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.TestHost;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Testcontainers.MsSql;
+using Xunit;
 
 namespace Shared;
 
